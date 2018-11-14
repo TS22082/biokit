926fc3987e40f99864452c1004251cb9ebaef77f
GeekCentric <ts22082@gmail.com>
2018-11-14 13:40:02 -0800

    export card

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import TextInput from './TextInput/TextInput';
 import Form from './Form/Form';
 import FormGroup from './FormGroup/FormGroup';
-import Card from './Card/Card'
+import Card from './Card/Card';
 
 export {TextInput, Form, FormGroup, Card}
