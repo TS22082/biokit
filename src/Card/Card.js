fb87f0605b186852c0dbdc28542b2c14f55be56a
GeekCentric <ts22082@gmail.com>
2018-11-14 13:39:32 -0800

    create card component

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,17 +1,21 @@
-class Card extends Component {
+/**
+ * @class Card
+ */
+
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+  state = {  }
   render() { 
     return ( 
-      <div>
-        <card className={`rounded-${this.props.rounded} ${this.props.spacing}`}>
-          <div className={`card-header bg-dark text-light rounded=${this.props.rounded}`}>
-            <h4 className="card-title mb-0">
-              {this.props.icon} {this.props.title}
-            </h4>
-          </div>
-          <div className='card-body'>{this.props.children}</div>
-        </card>
-      </div>);
+      <div className={`rounded-${this.props.rounded} ${this.props.spacing}`}>
+        <div className={`card-header bg-dark text-light rounded-${this.props.rounded}`}>
+          <h4 className="card-title mb-0">
+            {this.props.icon} {this.props.title}
+          </h4>
+        </div>
+        <div className="card-body">{this.props.children}</div>
+      </div>
+    );
   }
 }
-
-export default Card;
