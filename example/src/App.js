24b81f49d9d90ca242a6ab0b1d9ac3182139e401
GeekCentric <ts22082@gmail.com>
2018-11-14 13:38:27 -0800

    export Card Component

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {TextInput, Form, FormGroup} from 'biokit'
+import {TextInput, Form, FormGroup, Card} from 'biokit'
 
 export default class App extends Component {
   constructor(props) {
@@ -19,24 +19,30 @@ export default class App extends Component {
   };
   render () {
     return (
-      <Form sm={"12"} md={"12"} lg={"4"}>
-        <FormGroup>
-          <TextInput
-            inputName={"Name"}
-            type={"text"}
-            placeholder={"Please enter name"}
-            handleChange={this.handleNameChange}
-            text={this.state.name}
-          />
-          <TextInput
-            inputName={"Email"}
-            type={"email"}
-            placeholder={"Please enter name"}
-            handleChange={this.handleEmailChange}
-            text={this.state.email}
-          />
-        </FormGroup>
-      </Form>
+      <Card
+        rounded={'0'}
+        spacing={'mt-0'}
+        icon={<i className='mdi mdi-grid mr-2'/>}
+        title={'Container List'}>
+        <Form sm={'12'} md={'12'} lg={'4'}>
+          <FormGroup>
+            <TextInput
+              inputName={'Name'}
+              type={'text'}
+              placeholder={'Please enter name'}
+              handleChange={this.handleNameChange}
+              text={this.state.name}
+            />
+            <TextInput
+              inputName={'Email'}
+              type={'email'}
+              placeholder={'Please enter name'}
+              handleChange={this.handleEmailChange}
+              text={this.state.email}
+            />
+          </FormGroup>
+        </Form>
+      </Card>
     )
   }
 }
