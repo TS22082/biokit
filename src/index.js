0d56a9418aa0bf2db5c585b3dcd687cf0e852343
GeekCentric <ts22082@gmail.com>
2018-11-15 14:56:57 -0800

    export newly created components

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import Form from './Form/Form';
 import FormGroup from './FormGroup/FormGroup';
 import Card from './Card/Card';
 import Container from './Container/Container';
+import Row from './Row/Row';
+import Column from './Column/Column';
 
-export {TextInput, Form, FormGroup, Card, Container}
+export {
+  TextInput, 
+  Form, 
+  FormGroup, 
+  Card, 
+  Container, 
+  Row,
+  Column
+}
