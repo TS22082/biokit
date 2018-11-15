a02f1a82bff9f4ec1caae98df037f0324e25e948
GeekCentric <ts22082@gmail.com>
2018-11-15 14:28:59 -0800

    create Container component

---
+++ src/Container/Container.js
@@ -0,0 +1,23 @@
+/**
+ * @class Container
+ */
+
+import React, { Component } from 'react';
+
+export default class Container extends Component {
+  setClass = () => {
+    if (this.props.fluid === true) {
+      return ('container-fluid')
+    } else {
+      return('container')
+    }
+  }
+  state = {  }
+  render() {
+    return ( 
+      <div className={this.setClass}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
