e21afdb63d92160f78f74451e458d51738126b8c
GeekCentric <ts22082@gmail.com>
2018-11-15 14:55:33 -0800

    create Row component

---
+++ src/Row/Row.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Row extends Component {
+  state = {  }
+  render() { 
+    return (  
+      <div className='row'>
+        {this.props.children}
+      </div>
+    );
+  }
+}
