612c5df2dd00b6fce0499fd76e6ecf7e63416396
GeekCentric <ts22082@gmail.com>
2018-11-15 14:56:04 -0800

    display example

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {TextInput, Form, FormGroup, Card, Container} from 'biokit'
+import {TextInput, Form, FormGroup, Card, Container, Row, Column} from 'biokit'
 
 export default class App extends Component {
   constructor(props) {
@@ -21,30 +21,32 @@ export default class App extends Component {
     return (
       <Container
         fluid={false}>
-        <Card
-          rounded={'0'}
-          spacing={'mt-0'}
-          icon={<i className='mdi mdi-grid mr-2'/>}
-          title={'Container List'}>
-          <Form sm={'12'} md={'12'} lg={'4'}>
-            <FormGroup>
-              <TextInput
-                inputName={'Name'}
-                type={'text'}
-                placeholder={'Please enter name'}
-                handleChange={this.handleNameChange}
-                text={this.state.name}
-              />
-              <TextInput
-                inputName={'Email'}
-                type={'email'}
-                placeholder={'Please enter name'}
-                handleChange={this.handleEmailChange}
-                text={this.state.email}
-              />
-            </FormGroup>
-          </Form>
-        </Card>
+        <Column sm={'12'} md={'12'} lg={'12'}>
+          <Card
+            rounded={'0'}
+            spacing={'mt-0'}
+            icon={<i className='mdi mdi-grid mr-2'/>}
+            title={'Container List'}>
+            <Form>
+              <FormGroup>
+                <TextInput
+                  inputName={'Name'}
+                  type={'text'}
+                  placeholder={'Please enter name'}
+                  handleChange={this.handleNameChange}
+                  text={this.state.name}
+                />
+                <TextInput
+                  inputName={'Email'}
+                  type={'email'}
+                  placeholder={'Please enter name'}
+                  handleChange={this.handleEmailChange}
+                  text={this.state.email}
+                />
+              </FormGroup>
+            </Form>
+          </Card>
+        </Column>
       </Container>
     )
   }
